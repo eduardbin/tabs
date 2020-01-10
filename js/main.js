a54633f0db5a160feae48cf9b8be4a94f5f54fff
eduardbin <binukeduard@gmail.com>
2020-01-10 03:04:41 +0200

    add all html and styles, fix bugs, optimize js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,54 @@
 ; (function () {
 	"use strict";
-
+	//tab secrions
 	let tabItems = document.querySelectorAll('.tab-item');
-	let tabSections = document.querySelectorAll('.test');
-
-	for (let tabSection of tabSections) {
-		tabSection.hidden = true;
-	}
+	let tabSections = document.querySelectorAll('.ba-section');
 
 	for (let tabItem of tabItems) {
-		tabItem.addEventListener('click', (event) => {
-			event.preventDefault()
-		});
-		tabItem.addEventListener('focus', showSection);
-		tabItem.addEventListener('blur', hideSection);
+		tabItem.addEventListener('click', (event) => event.preventDefault());
+		tabItem.addEventListener('click', getSelectedSection);
 	};
 
-	function showSection() {
+	function getSelectedSection() {
 		let itemId = this.hash;
 		let section = document.querySelector(`${itemId}`);
+
+		hideSections();
+		showSelectedSection(section);
+	};
+
+	function hideSections() {
+		for (let tabSection of tabSections) {
+			tabSection.hidden = true;
+		};
+	};
+
+	function showSelectedSection(section) {
 		section.hidden = false;
 	};
 
-	function hideSection() {
-		let itemId = this.hash;
-		let section = document.querySelector(`${itemId}`);
-		section.hidden = true;
+	//tab styles
+	let navItems = document.querySelectorAll('.ba-nav__item');
+
+	for (let navItem of navItems) {
+		navItem.addEventListener('click', addActiveClass);
 	};
 
+	function addActiveClass() {
+		removeAllActiveClasses();
+
+		this.classList.add('active');
+	};
+
+	function removeAllActiveClasses() {
+		for (let navItem of navItems) {
+			navItem.classList.remove('active');
+		};
+	};
+
+	//set first section active
 	window.addEventListener('load', () => {
-		tabItems[0].focus();
+		tabItems[0].click();
 	});
 })();
 
